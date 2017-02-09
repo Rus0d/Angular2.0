5d2cc95238ce93e03230fbaf309447f69b1828d1
Rus0d <rusya.liamg@gmail.com>
2017-02-09 15:08:49 +0200

    lesson 7

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
 
-const todos = [
-    'Изучить JavaScript',
-    'Изучить Angular 2',
-    'Написать приложение',
+/*interface ITodo {
+    title: string;
+    completed: boolean;
+}*/
+
+/*class Todo {
+    title: string;
+    completed: boolean;
+
+    constructor(title: string, completed: boolean = false) {
+        this.title = title;
+        this.completed = completed;
+    }
+}*/
+
+class Todo {
+    constructor(public title: string,
+                public completed: boolean = false) {}
+}
+
+/*const todos: ITodo[] = [*/
+const todos: Todo[] = [
+    {
+        title: 'Изучить JavaScript',
+        completed: true
+    },
+    {
+        title: 'Изучить Angular 2',
+        completed: false
+    },
+    {
+        title: 'Написать приложение',
+        completed: false
+    }
 ];
 
 @Component({
@@ -13,6 +43,31 @@ const todos = [
     styleUrls: ['app.component.css']
 })
 export class AppComponent {
-    title = 'Angular 2Do';
-    todos = todos;
+    title: string = 'Angular 2Do';
+    /*todos: ITodo[] = todos;*/
+    todos: Todo[] = todos;
+    newTodoTitle: string = '';
+
+    create() {
+        let todo: Todo = new Todo(this.newTodoTitle);
+
+        this.todos.push(todo);
+        this.newTodoTitle = '';
+    }
+
+    /*toggle(todo: { title: string, completed: boolean }) {*/
+    /*toggle(todo: ITodo) {*/
+    toggle(todo: Todo) {
+        todo.completed = !todo.completed;
+    }
+
+    /*delete(todo: { title: string, completed: boolean }) {*/
+    /*delete(todo: ITodo) {*/
+    delete(todo: Todo) {
+        let index = this.todos.indexOf(todo);
+
+        if (index > -1) {
+            this.todos.splice(index, 1)
+        }
+    }
 }
